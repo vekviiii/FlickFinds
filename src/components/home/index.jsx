dd26b4d874888c4c145b9a0b892714182dd93859
vekviiii <vivekkumar124356@gmail.com>
2024-07-21 14:47:22 +0530

    Added a mock data and made few UI changes

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Home = () => {
+  const [movie, setMovie] = useState("");
+
+  const DisplayMovie = () => {
+    setMovie("hello!");
+  };
+
   return (
     <>
-      <div className="container d-flex justify-content-center my-5">
-        <input id="movie" placeholder="Enter movie name" type="text" />
-      </div>
-      <div className="container d-flex justify-content-center my-5">
-        <button id="submit">Submit</button>
+      <div className="container" style={{ height: "75vh" }}>
+        <div
+          className="d-flex justify-content-center align-items-end"
+          style={{ height: "60%" }}
+        >
+          <button id="submit" onClick={DisplayMovie}></button>
+        </div>
+        <div className="my-4 text-center">
+          <label>{movie}</label>
+        </div>
       </div>
     </>
   );
